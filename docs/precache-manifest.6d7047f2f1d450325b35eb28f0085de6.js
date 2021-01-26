7b20e62758d11c71e0408c584178b4ae613e690c
Juliette Hoisnard <juliette@hoisnard.fr>
2021-01-26 13:05:35 +0100

    Add docs

---
+++ docs/precache-manifest.6d7047f2f1d450325b35eb28f0085de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7c9d1cf3819d19251e2c88e5a438e0",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7e63c0010e3752503306",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e63c0010e3752503306",
+    "url": "/tic-tac-toe/static/js/main.13f4a0b7.chunk.js"
+  },
+  {
+    "revision": "4ad95f08d6b4d56d4069",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa7aabe4.js"
+  }
+]);
